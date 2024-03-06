18259815ba9e8d358651940798266a86ad5f2aa3
BernoldAZ <41762420+BernoldAZ@users.noreply.github.com>
2024-03-06 21:41:45 +0100

    Events added

--- schedule.js
+++ schedule.js
@@ -8,21 +8,21 @@ function closeForm() {
 }
 
 function addAppoiment() {
-  var name = (document.getElementById("name").value);
+  let name = (document.getElementById("name").value);
 
-  var start = (document.getElementById("start").value);
-  var minute_start = parseInt(start[3] + start[4]) /60;
-  var hour_start = parseInt(start[0] + start[1]) - 8 + minute_start;
-  var hour_top = hour_start*100 + hour_start*4;
+  let start = (document.getElementById("start").value);
+  let minute_start = parseInt(start[3] + start[4]) /60;
+  let hour_start = parseInt(start[0] + start[1]) - 8 + minute_start;
+  let hour_top = hour_start*100 + hour_start*4;
 
-  var end = (document.getElementById("end").value);
-  var minute_end = parseInt(end[3] + end[4]) /60;
-  var hour_end = parseInt(end[0] + end[1]) - 8 + minute_end;
-  var hour_botton = (hour_end-hour_start)*100 + hour_end*3;
+  let end = (document.getElementById("end").value);
+  let minute_end = parseInt(end[3] + end[4]) /60;
+  let hour_end = parseInt(end[0] + end[1]) - 8 + minute_end;
+  let hour_botton = (hour_end-hour_start)*100 + (hour_end-hour_start)*3;
 
 
-  var days_name = ["Monday","Tuesday","Wednesday","Thursday","Friday"];
-  var days_selected = [0,0,0,0,0];
+  let days_name = ["Monday","Tuesday","Wednesday","Thursday","Friday"];
+  let days_selected = [0,0,0,0,0];
 
   if(document.getElementById("MondayCheck").checked == true){days_selected[0] = 1};
   if(document.getElementById("TuesdayCheck").checked == true){days_selected[1] = 1};
@@ -30,20 +30,13 @@ function addAppoiment() {
   if(document.getElementById("ThursdayCheck").checked == true){days_selected[3] = 1};
   if(document.getElementById("FridayCheck").checked == true){days_selected[4] = 1};
 
-  var days_appoiment = "";
-  for (let index = 0; index < days_selected.length; index++) {
-    if(days_selected[index] == 1){days_appoiment = days_appoiment + days_name[index] + " "}
-  }
+  let days_appoiment = "";
   
-  var appoiment = "Appoiment: " + name + ", Start: " + start + ", Finish: " + end + ", Days: " + days_appoiment + " Added";
-
-  window.alert(appoiment);
-
-
   for (let index = 0; index < days_selected.length; index++) {
     if(days_selected[index] == 1){
+      days_appoiment = days_appoiment + days_name[index] + " "
       // create a new div element
-      var div = document.createElement("div");
+      let div = document.createElement("div");
       div.style.position = "absolute";
       div.style.display = "block";
       div.style.width = "100%";
@@ -56,4 +49,22 @@ function addAppoiment() {
       document.getElementById(days_name[index]).appendChild(div);
     }
   }
+
+  let appoiment = "Appoiment: " + name + ", Start: " + start + ", Finish: " + end + ", Days: " + days_appoiment + " Added";
+  window.alert(appoiment);
+}
+
+function warning() {
+  return "You will lose your appoiments";
+}
+
+function message(){
+  window.alert("Just one click is enough");
+}
+
+function newPointer(x) {
+  x.style.cursor = "pointer";
+}
+function oldPointer(x) {
+  x.style.cursor = "default";
 }
